eb25c1f761ffa78863e24800334d9f5d2bd14894
339610123 <339610123@gotvdsb.ca>
2025-06-04 10:45:07 -0400

    Basic Attack
    simple dice roll combat

---
+++ Force.py
@@ -0,0 +1,24 @@
+import random
+class Force:
+    units = 0
+    def __init__(self, unit):
+        self.units = unit
+        print("units: ", self.units)
+
+    # roll dice and see who wins
+    def Attack(self, p2):
+        dice1 = random.randint(1 ,6)
+        dice2 = random.randint(1, 6)
+
+        if (dice1 == dice2):
+            return self.Attack(p2)
+
+        # player 1 dice is higher
+        if (dice1 > dice2):
+            p2.units -= 1
+            print("player 1 wins (p1: %s units left, p2: %s units left)" % (self.units, p2.units))
+
+        # player 2 dice is higher
+        if (dice2 > dice1):
+            p1.units -= 1
+            print("player 2 wins (p1: %s units left, p2: %s units left)" % (self.units, p2.units))
