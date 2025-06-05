d621ae163bca566d9cb935d3b6dee1d30f135c05
DEVAN-LUCAS <339595746@GOTVDSB.CA>
2025-06-05 10:39:38 -0400

    Update Complete code.py

--- Complete code.py
+++ Complete code.py
@@ -1,40 +1,97 @@
 from pynput.keyboard import Key, Controller
 import random
+
 class Force:
     units = 0
+
     def __init__(self, unit):
         self.units = unit
         print("units: ", self.units)
 
-    # roll dice and see who wins
     def Attack(self, p2):
-        dice1 = random.randint(1 ,6)
+        dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
 
-        if (dice1 == dice2):
+        if dice1 == dice2:
             return self.Attack(p2)
 
-        # player 1 dice is higher
-        if (dice1 > dice2):
+        if dice1 > dice2:
             p2.units -= 1
             print("player 1 wins (p1: %s units left, p2: %s units left)" % (self.units, p2.units))
-
-        # player 2 dice is higher
-        if (dice2 > dice1):
+        else:
             self.units -= 1
             print("player 2 wins (p1: %s units left, p2: %s units left)" % (self.units, p2.units))
 
-def main_Menu_Header():
-    program_name = "Welcome To Conquest: A Territory Strategy Game!"
-    version = "1.0"
-    header_text = f"{program_name} - Version {version}"
-    header_width = 100
-    print()
-    print("=" * header_width)
-    print(header_text.center(header_width))
-    print("=" * header_width)
-    print()
+class Territories:
+    territoryCord = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
 
+    @staticmethod
+    def territory_claim(gameMap, x, y, oldChar, newChar):
+        mapWidth = len(gameMap)
+        mapHeight = len(gameMap[0])
+
+        if oldChar is None:
+            oldChar = gameMap[x][y]
+
+        if gameMap[x][y] != oldChar:
+            return
+        gameMap[x][y] = newChar
+
+        if x > 0:
+            Territories.territory_claim(gameMap, x - 1, y, oldChar, newChar)
+        if y > 0:
+            Territories.territory_claim(gameMap, x, y - 1, oldChar, newChar)
+        if x < mapWidth - 1:
+            Territories.territory_claim(gameMap, x + 1, y, oldChar, newChar)
+        if y < mapHeight - 1:
+            Territories.territory_claim(gameMap, x, y + 1, oldChar, newChar)
+
+    @staticmethod
+    def is_adjacent(gameMap, xCoor, yCoor):
+        for idx in range(-2, 3):
+            for idx2 in range(-2, 3):
+                nx, ny = xCoor + idx, yCoor + idx2
+                if 0 <= nx < len(gameMap) and 0 <= ny < len(gameMap[0]):
+                    if gameMap[nx][ny] == "-":
+                        return True
+        return False
+
+    @staticmethod
+    def can_steal_territory(gameMap):
+        for x in range(worldWidth):
+            for y in range(worldHeight):
+                if gameMap[x][y] == "+":
+                    if Territories.is_adjacent(gameMap, x, y):
+                        return True
+        return False
+
+    @staticmethod
+    def territory_steal(gameMap, xHave, yHave, xTake, yTake):
+        taked = gameMap[xHave][yHave]
+        taker = gameMap[xTake][yTake]
+        Territories.territory_claim(gameMap, xHave, yHave, None, '+')
+        Territories.territory_claim(gameMap, xTake, yTake, None, '-')
+        if Territories.can_steal_territory(gameMap):
+            Territories.territory_claim(gameMap, xTake, yTake, None, taker)
+            Territories.territory_claim(gameMap, xHave, yHave, None, taker)
+        else:
+            Territories.territory_claim(gameMap, xTake, yTake, None, taked)
+            Territories.territory_claim(gameMap, xHave, yHave, None, taker)
+
+class intro:
+    @staticmethod
+    def main_Menu_Header():
+        program_name = "Welcome To Conquest: A Territory Strategy Game!"
+        version = "1.0"
+        header_text = f"{program_name} - Version {version}"
+        header_width = 100
+        print()
+        print("=" * header_width)
+        print(header_text.center(header_width))
+        print("=" * header_width)
+        print()
+
+    @staticmethod
     def show_instructions():
         program_name = "Instructions on How to Play Conquest!"
         header_text = f"{program_name}"
@@ -50,28 +107,31 @@ def main_Menu_Header():
             print(line)
         print()
 
+    @staticmethod
     def main_Menu():
-        choice = input("Would you like to see the instructions? (yes/no): ").strip()
-        if choice == "yes" or choice == "Yes" or choice == "Y" or choice == "y":
-            show_instructions()
-        elif choice == "no" or choice == "No" or choice == "N" or choice == "n":
-            print("\nAlright, let's proceed to the game!\n")
-        
-        else: 
-            print("Enter a valid input.")
+        validInput = False
+        while not validInput:
+            choice = input("Would you like to see the instructions? (yes/no): ").strip()
+            if choice.lower() in ["yes", "y"]:
+                intro.show_instructions()
+                validInput = True
+            elif choice.lower() in ["no", "n"]:
+                print("\nAlright, let's proceed to the game!\n")
+                validInput = True
+            else:
+                print("Enter a valid input.")
 
-    main_Menu()
-
-main_Menu_Header()
-
-def checkWinner (plyrs):
+def checkWinner(plyrs):
     for idx in range(len(plyrs)):
         if winner:
-            return True, player[idx]
+            return True, plyrs[idx]
     return False, None
 
-#MAIN PGM
-validInputPlayerCount = 0
+# ===== MAIN PROGRAM =====
+validInputPlayerCount = False
+intro.main_Menu_Header()
+intro.main_Menu()
+
 while not validInputPlayerCount:
     playerCount = input("How many players will be playing? (2-4): ")
     try:
@@ -79,10 +139,39 @@ while not validInputPlayerCount:
     except:
         print("Invalid player count, try again!")
     else:
-        if playerCount >4 or playerCount <2:
+        if playerCount > 4 or playerCount < 2:
             print("Invalid player count, try again!")
         else:
             validInputPlayerCount = True
+
+player = [None] * playerCount
+order = list(range(playerCount))
+playerAssignments = "ABCD"
+
+for idx in range(playerCount):
+    player[idx] = input("What is the name of player %d?: " % (idx + 1))
+
+random.shuffle(order)
+print("The order of play will be:")
+for idx in range(playerCount):
+    print("player %s: %s" % (playerAssignments[idx], player[order[idx]]))
+
+input("Click enter to play")
+turn = 0
+gameComplete = False
+
+# Placeholder values for map size and winner check
+worldWidth = 10
+worldHeight = 10
+winner = False
+
+while not gameComplete:
+    personsTurn = turn % playerCount
+    playerInUse = player[personsTurn]
+    print("It's", playerInUse + "'s turn.")
+    turn += 1
+
+
 player = [None] * playerCount
 order = list(range(0, playerCount))
 for idx in range(playerCount):
