17015c839ecb5bf7e37fdf90906b82a2e4cf692a
DEVAN-LUCAS <339595746@GOTVDSB.CA>
2025-06-16 13:06:23 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,77 +8,28 @@
 # 2025-06-10: Created file
 # 2025-06-11: added opening save file
 #######################
-
-import MainMenu
-import GamePlay
-import player
-
-class masterPlayer:
-    ord = 0
-    playerClass = ""
-    #force = Force.Force
-
-    def __init__(self, order, saveName):
-        self.ord = order
-        #self.playerClass = self.askName()
-        self.checkSave(saveName)
-
-    # check if a save has been loaded
-    def checkSave(self, saveName):
-        # might not be finsished yet
-        try:
-            save1 = open("saveName",'x')
-        except FileExistsError:
-            return True
-        return False
-
-    def __str__(self):
-        return self.playerClass
-
-def openSave():
-    print("Do you want to open a save file (y or n): ", end="")
-    des = input()
-    if (des == 'y'):
-        print("loadSave")
-
-        try: 
-            fileNum = int(input("Which save do you want to open (1-3): "))
-        except ValueError:
-            print("Invalid save number, try again")
-            return openSave()
-        else: 
-            if (0 < fileNum < 4):
-                fileName = "save" + fileNum
-                return fileName
-            else:
-                print("Number is out of range, try again")
-                return openSave()
-        
-    elif (des == "n"):
-        print("Make new game")
-        return
-    else:
-        print("invalid input, try again")
-        return openSave()
-
-
-openSave()
-player = masterPlayer(1)
-###############################################
 import MainMenu
 import GamePlay
 import sys
 import time
 import random
 import os
+def typewriterTxt(msg, timeTake):
+            for char in msg:
+                sys.stdout.write(char)
+                sys.stdout.flush()
+                time.sleep(random.randint(0,10)/100*timeTake)
 MainMenu.initialization_of_game()
 time.sleep(0.5)
 os.system('cls')
+typewriterTxt("initializing game...",2)
+os.system('cls')
+typewriterTxt("Making map",2)
+os.system('cls')
+typewriterTxt("Loading files...",2)
+os.system('cls')
 print("Game Starting, goodluck to all and  have fun!\n")
-for char in "█████████████████████████████████████████████\n":
-                sys.stdout.write(char)
-                sys.stdout.flush()
-                time.sleep(random.randint(1,10)/50)
+typewriterTxt("█████████████████████████████████████████████\n", 5)
 os.system('cls')
 time.sleep(1)               
 GamePlay.gamePlay()
