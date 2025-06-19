b23f361ef543406a5f0da9856fedb8fd75d15c09
DEVAN-LUCAS <339595746@GOTVDSB.CA>
2025-06-18 22:51:08 -0400

    Update saveFile.py

--- saveFile.py
+++ saveFile.py
@@ -263,7 +263,7 @@ class SaveForce:
         elif (des == 'n'): 
             des1=""
             while (des1 != "y" or des1 != 'n'):
-                des1 = input("Cancelled, do you want to buy troops still? (y or n): ")
+                des1 = input("Cancelled, do you want to buy still? (y or n): ")
                 if (des1 == 'y'):
                     return self.buyStrength(playerClass)
                 if (des1 == 'n'):
@@ -312,7 +312,7 @@ class SaveForce:
         elif (des == 'n'): 
             des1=""
             while (des1 != "y" or des1 != 'n'):
-                des1 = input("Cancelled, do you want to buy troops still? (y or n): ")
+                des1 = input("Cancelled, do you want to buy still? (y or n): ")
                 if (des1 == 'y'):
                     return self.buyDefense(playerClass)
                 if (des1 == 'n'):
